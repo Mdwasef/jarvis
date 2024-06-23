f323d7efb25c77c85a60fa6f08959bdfeeaea498
Mdwasef <133320753+Mdwasef@users.noreply.github.com>
2024-06-23 22:31:30 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,9 @@
+music={
+
+"kaha":"https://youtu.be/AX6OrbgS8lI?si=qAZr7FvNYlya5XV4",
+
+"king":"https://youtu.be/iSxRVXBQ8rQ?si=IPSGPdZtALk9UHtA",
+
+"undertaker":"https://youtu.be/nGh5D-wUicE?si=VjDZcPZ4B8FRAgtg",
+
+}
